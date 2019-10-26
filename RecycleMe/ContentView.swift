494e275b14327c2a5fdfe42bbd5bfee11a862382
Daniel8473 <Dansilva@ucdavis.edu>
2019-10-26 01:24:02 -0700

    Initial Commit

---
+++ RecycleMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecycleMe
+//
+//  Created by Daniel on 10/26/19.
+//  Copyright © 2019 CalHacks6. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
